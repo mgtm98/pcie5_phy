9a25053e94ac3ef10aceb2a633084f89395e091e
Mohamed Abdullah <mas_seyam@yahoo.com>
2021-01-29 17:39:52 +0200

    pipe_driver_bfm methods prototype Added the tasks declartion to the interface

--- tb/agents/pipe_agent/pipe_driver_bfm.sv
+++ tb/agents/pipe_agent/pipe_driver_bfm.sv
@@ -2,6 +2,7 @@ interface pipe_driver_bfm(
   input bit clk,
   input bit reset,
   // TODO Change the size of RxData, RxDataK, TxData, TxDataK
+  // Parametrization not mentioned in the document
   output logic [31:0] RxData,
   output logic [3:0] RxDataK,
   output logic RxValid,
@@ -24,5 +25,43 @@ interface pipe_driver_bfm(
   input logic [3:0] TxSynchHeader,
   input logic [3:0] Powerdown
 );
+
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+import pipe_agent_pkg::*;
+
   
+//------------------------------------------
+// Data Members
+//------------------------------------------
+
+//------------------------------------------
+// Methods
+//------------------------------------------
+task link_up(device_config_s device_config);
+  //to be implemented
+endtask
+
+
+task send_tlp(tlp_s tlp);
+  //to be implemented
+endtask
+
+
+task send_dllp(dllp_s dllp);
+  //to be implemented
+endtask
+
+
+task change_state(state_e state);
+  //to be implemented
+endtask
+
+task change_speed(speed_mode_e speed);
+  //to be implemented
+endtask
+
+
+
 endinterface
