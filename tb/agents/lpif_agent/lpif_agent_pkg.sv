86f144e18dbcc68c9410a017e3a8146631fb9f7c
mennnatullah <78158360+mennnatullah@users.noreply.github.com>
2021-01-29 23:29:48 +0200

    Update lpif_agent_pkg.sv

---
+++ tb/agents/lpif_agent/lpif_agent_pkg.sv
@@ -0,0 +1,16 @@
+package lpif_agent_pkg;
+
+import uvm_pkg::*;
+
+`include "uvm_macros.svh"
+`include "config_macro.svh"
+`include "lpif_seq_item.svh"
+`include "lpif_agent_config.svh"
+`include "lpif_driver.svh"
+`include "lpif_coverage_monitor.svh"
+`include "lpif_monitor.svh"
+`include "lpif_agent.svh"
+
+typedef uvm_sequencer#(lpif_seq_item) lpif_sequencer;
+
+endpackage: lpif_agent_pkg
