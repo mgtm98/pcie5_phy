5127636f9311af6561116127764df62fa2306553
Mohamed Gamal <mgtmprog@gmail.com>
2021-02-01 07:56:39 +0200

    common enumerates needed by the entire TB

---
+++ tb/agents/common_pkg.sv
@@ -0,0 +1,17 @@
+//  Package: common_pkg
+//  All common types used by the agents are defined here
+
+package common_pkg;
+
+  // Pipe bus width between the MAC layer and PHY layer
+  // width can either be 32bit - 16bit - 8bit
+  // for more info check Pipe interface refence Page 18 
+  typedef enum {
+    BUS_WIDTH_8  = 8,
+    BUS_WIDTH_16 = 16,
+    BUS_WIDTH_32 = 32
+  } pipe_data_width_t;
+   
+endpackage: common_pkg
+
+
