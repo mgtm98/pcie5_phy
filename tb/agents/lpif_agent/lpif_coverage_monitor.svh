24fcbe6bb96795080173279ea33633721685eadc
theMeanScore <m.ad.ali2165@gmail.com>
2021-02-07 03:14:15 +0200

    make an instance from the covergroup

--- tb/agents/lpif_agent/lpif_coverage_monitor.svh
+++ tb/agents/lpif_agent/lpif_coverage_monitor.svh
@@ -1,3 +1,4 @@
+// TODO modify the structure for the 2 analysis ports
 class lpif_coverage_monitor extends uvm_subscriber #(lpif_seq_item);
 
   `uvm_component_utils(lpif_coverage_monitor)
@@ -10,6 +11,7 @@ class lpif_coverage_monitor extends uvm_subscriber #(lpif_seq_item);
 
   function new(string name = "lpif_coverage_monitor", uvm_component parent = null);
     super.new(name, parent);
+    lpif_seq_item_cov = new;
   endfunction
 
   function void write(lpif_seq_item lpif_seq_item_h);
