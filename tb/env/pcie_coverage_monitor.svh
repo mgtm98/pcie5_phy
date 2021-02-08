75d00a9588dfc4741e96aa6b2fbcb91afe642f77
LobnaTarek <lobna.tarek.w@gmail.com>
2021-02-08 14:57:41 +0200

    I added constructor for the covergroup

--- tb/env/pcie_coverage_monitor.svh
+++ tb/env/pcie_coverage_monitor.svh
@@ -26,6 +26,7 @@ endclass: pcie_coverage_monitor
 
 function pcie_coverage_monitor::new(string name = "pcie_coverage_monitor", uvm_component parent = null);
   super.new(name, parent);
+  pcie_coverage_monitor_cov = new("pcie_coverage_monitor_cov",this);
 endfunction
 
 function void pcie_coverage_monitor::build_phase(uvm_phase phase);
