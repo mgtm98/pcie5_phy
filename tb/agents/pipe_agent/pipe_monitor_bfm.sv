ef1b8029a157632b3dc005a0609c753c0348966b
Nada <nadatarek_14@hotmail.com>
2021-02-09 15:00:57 +0200

    modified

--- tb/agents/pipe_agent/pipe_monitor_bfm.sv
+++ tb/agents/pipe_agent/pipe_monitor_bfm.sv
@@ -4,26 +4,43 @@ interface pipe_monitor_bfm (
 
   input logic PCLK,
   //RxData,RxDataK,TxData and TxDataK sizes can be changed according to interface size
-  input logic [31:0] RxData,   //for 32 bit interface
-  input logic [3:0]  RxDataK,  //for 32 bit interface
-  input logic        RxValid,
-  input logic        PhyStatus,
-  input logic        RxElecidle,
-  input logic [2:0]  RxStatus, 
-  input logic        RxstartBlock, 
-  input logic [3:0]  RxsynchHeader,
-  input logic [31:0] TxData,  //for 32 bit interface
-  input logic [3:0]  TxDataK, //for 32 bit interface
-  input logic        TxDataValid,
-  input logic        TxDetectRx,
-  input logic [3:0]  TxEelecIdle,
-  input logic [1:0]  Width,
-  input logic [3:0]  Rate, 
-  input logic [3:0]  PCLKRate, 
-  input logic        Reset,
-  input logic        TxstartBlock,
-  input logic [3:0]  TxsynchHeader,
-  input logic [3:0]  Powerdown
+  input logic [31:0] rx_data,   //for 32 bit interface
+  input logic [3:0]  rx_data_K,  //for 32 bit interface
+  input logic        rx_valid,
+  input logic        phy_status,
+  input logic        rx_elec_idle,
+  input logic [2:0]  rx_status, 
+  input logic        rx_start_block, 
+  input logic [3:0]  rx_synch_header,
+  input logic [31:0] tx_data,  //for 32 bit interface
+  input logic [3:0]  tx_data_K, //for 32 bit interface
+  input logic        tx_data_valid,
+  input logic        tx_detect_rx,
+  input logic [3:0]  tx_elec_idle,
+  input logic [1:0]  width,
+  input logic [3:0]  rate, 
+  input logic [3:0]  pclk_rate, 
+  input logic        reset,
+  input logic        tx_start_block,
+  input logic [3:0]  tx_synch_header,
+  input logic [3:0]  power_down
 );
+
+import uvm_pkg::*;
+import pipe_agent_pkg::*;
+
+pipe_monitor proxy;
+
+initial
+begin
+  forever
+  begin
+    @($rose(rx_valid))
+    begin
+      `uvm_info("pipe_monitor_bfm", "dummy seq_item detected", UVM_MEDIUM)
+      proxy.pipe_monitor_dummy();
+    end
+  end
+end
   
 endinterface
