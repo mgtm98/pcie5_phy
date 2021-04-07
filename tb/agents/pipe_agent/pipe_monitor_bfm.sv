6cab304b587d95f348d18616af99e812e2f70d90
ibrahemkhafaga <77680175+ibrahemkhafaga@users.noreply.github.com>
2021-04-07 21:33:21 +0200

    Update pipe_monitor_bfm.sv
    signals updated

--- tb/agents/pipe_agent/pipe_monitor_bfm.sv
+++ tb/agents/pipe_agent/pipe_monitor_bfm.sv
@@ -1,30 +1,40 @@
 interface pipe_monitor_bfm (
-  input bit clk, 
-  // input bit reset, 
-
-  input logic PCLK,
-  //RxData,RxDataK,TxData and TxDataK sizes can be changed according to interface size
-  input logic [31:0] rx_data,   //for 32 bit interface
-  input logic [3:0]  rx_data_k,  //for 32 bit interface
-  input logic        rx_valid,
-  input logic        phy_status,
-  input logic        rx_elec_idle,
-  input logic [2:0]  rx_status, 
-  input logic        rx_start_block, 
-  input logic [3:0]  rx_synch_header,
+//Tx
   input logic [31:0] tx_data,  //for 32 bit interface
-  input logic [3:0]  tx_data_k, //for 32 bit interface
   input logic        tx_data_valid,
-  input logic        tx_detect_rx,
-  input logic [3:0]  tx_elec_idle,
-  input logic [1:0]  width,
-  input logic [3:0]  rate, 
-  input logic [3:0]  pclk_rate, 
-  input logic        pclk,
-  input logic        reset,
+  input logic        tx_elec_idle,
+  input logic [3:0]  tx_data_k, //for 32 bit interface
   input logic        tx_start_block,
-  input logic [3:0]  tx_synch_header,
-  input logic [3:0]  power_down
+  input logic [1:0]  tx_synch_header,  
+  input logic        tx_detect_rx,
+  
+  //Rx
+  output logic [31:0] rx_data,   //for 32 bit interface
+  output logic        rx_data_valid,
+  output logic        rx_elec_idle,
+  output logic [3:0]  rx_data_k,  //for 32 bit interface
+  output logic        rx_start_block, 
+  output logic [1:0]  rx_synch_header,
+  output logic        rx_valid,
+  output logic [2:0]  rx_status, 
+  input  logic        rx_stand_by,
+  output logic        rx_stand_by_status,
+  
+  //Commands and Status signals
+  input logic [3:0]  power_down,
+  input logic [3:0]  rate, 
+  input logic [3:0]  phy_mode,  //=0  means PCIe
+  output logic       phy_status,
+  input logic [1:0]  width,
+  input logic [4:0]  pclk_rate,
+  input logic        pclk_change_ack,
+  output logic       pclk_change_ok,
+  
+  //clk and reset
+  input logic        pclk,
+  input logic        reset
+
+
 );
 
   `include "uvm_macros.svh"
