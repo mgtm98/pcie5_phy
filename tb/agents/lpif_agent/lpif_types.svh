b9968950fd2f80d824ef820b6e94c400624d043c
Mohamed Gamal <mgtmprog@gmail.com>
2021-04-16 13:42:05 +0200

    reformatting file

--- tb/agents/lpif_agent/lpif_types.svh
+++ tb/agents/lpif_agent/lpif_types.svh
@@ -1,20 +1,31 @@
 `ifndef __LPIF_TYPES_SVH
 `define __LPIF_TYPES_SVH
 
-typedef enum
-{LINK_UP, 
-RESET, 
-TLP_TRANSFER, 
-DLLP_TRANSFER, 
-ENTER_RETRAIN, 
-ENTER_L0S, 
-EXIT_L0S, 
-SPEED_MODE_CHANGE} lpif_operation_t;
+typedef enum{
+  LINK_UP, 
+  RESET, 
+  TLP_TRANSFER, 
+  DLLP_TRANSFER, 
+  ENTER_RETRAIN, 
+  ENTER_L0S, 
+  EXIT_L0S, 
+  SPEED_MODE_CHANGE
+} lpif_operation_t;
 
-typedef bit [3:0] enum {
-	ACTIVE = 4'b0001,
-	LINK_RESET = 4'b1001,
-	RETRAIN = 4'b1011
-} lpif_state_t;
+typedef enum{
+  NOP,
+  Active,
+  Active_L0s,
+  Deepest_Allowable_PM_State,  // [L1_Substates_only],
+  L1_1,
+  L1_2,
+  L1_3,
+  L1_4,
+  L2,
+  LinkReset,
+  Reserved,
+  Retrain,
+  Disable
+} lpif_states;
 
 `endif
