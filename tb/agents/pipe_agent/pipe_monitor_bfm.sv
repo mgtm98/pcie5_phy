499424d10ca8690a186b9c4b3aa41946c53a8e9c
Mohamed Gamal <mgtmprog@gmail.com>
2021-04-23 13:19:33 +0200

    added the parametrization for pipe

--- tb/agents/pipe_agent/pipe_monitor_bfm.sv
+++ tb/agents/pipe_agent/pipe_monitor_bfm.sv
@@ -1,40 +1,61 @@
-interface pipe_monitor_bfm (
-//Tx
-  input logic [31:0] tx_data,  //for 32 bit interface
-  input logic        tx_data_valid,
-  input logic        TxElecIdle,
-  input logic [3:0]  tx_data_k, //for 32 bit interface
-  input logic        tx_start_block,
-  input logic [1:0]  tx_synch_header,  
-  input logic        TxDetectRx,
+interface pipe_monitor_bfm 
+#(
+  localparam bus_data_width_param       = `PCIE_LANE_NUMBER  * `PIPE_MAX_WIDTH - 1,  
+  localparam bus_data_kontrol_param     = (`PIPE_MAX_WIDTH / 8) * `PCIE_LANE_NUMBER - 1
+)(  
+  input bit   clk,
+  input bit   reset,
+  input logic phy_reset,
+   
+  /*************************** RX Specific Signals *************************************/
+  input logic [bus_data_width_param:0]      rx_data,    
+  input logic [`PCIE_LANE_NUMBER-1:0]       rx_data_valid,
+  input logic [bus_data_kontrol_param:0]    rx_data_k,
+  input logic [`PCIE_LANE_NUMBER-1:0]       rx_start_block,
+  input logic [2*`PCIE_LANE_NUMBER-1:0]     rx_synch_header,
+  input logic [`PCIE_LANE_NUMBER-1:0]       rx_valid,
+  input logic [3*`PCIE_LANE_NUMBER-1:0]     rx_status,
+  input logic                               rx_elec_idle,
+  /*************************************************************************************/
   
-  //Rx
-  output logic [31:0] rx_data,   //for 32 bit interface
-  output logic        rx_data_valid,
-  output logic        rx_elec_idle,
-  output logic [3:0]  rx_data_k,  //for 32 bit interface
-  output logic        rx_start_block, 
-  output logic [1:0]  rx_synch_header,
-  output logic        rx_valid,
-  output logic [2:0]  rx_status, 
-  input  logic        rx_stand_by,
-  output logic        rx_stand_by_status,
-  
-  //Commands and Status signals
-  input logic [3:0]  PowerDown,
-  input logic [3:0]  rate, 
-  input logic [3:0]  phy_mode,  //=0  means PCIe
-  output logic       PhyStatus,
-  input logic [1:0]  width,
-  input logic [4:0]  pclk_rate,
-  input logic        pclk_change_ack,
-  output logic       pclk_change_ok,
-  
-  //clk and reset
-  input logic        pclk,
-  input logic        reset
+  /*************************** TX Specific Signals *************************************/
+  input logic [bus_data_width_param:0]      tx_data,    
+  input logic [`PCIE_LANE_NUMBER-1:0]       tx_data_valid,
+  input logic [bus_data_kontrol_param:0]    tx_data_k,
+  input logic [`PCIE_LANE_NUMBER-1:0]       tx_start_block,
+  input logic [2*`PCIE_LANE_NUMBER-1:0]     tx_synch_header,
+  input logic [`PCIE_LANE_NUMBER-1:0]       tx_elec_idle,
+  input logic [`PCIE_LANE_NUMBER-1:0]       tx_detect_rx__loopback,
+  /*************************************************************************************/
 
+  /*********************** Comands and Status Signals **********************************/
+  input logic [3:0]                         power_down,
+  input logic [3:0]                         rate,
+  input logic                               phy_status,
+  input logic [1:0]                         width,
+  input logic                               pclk_change_ack,
+  input logic                               pclk_change_ok,
+  /*************************************************************************************/
+  
+  /******************************* Message Bus Interface *******************************/
+  input logic [7:0]                         m2p_message_bus,
+  input logic [7:0]                         p2m_message_bus,
+  /*************************************************************************************/
 
+  /******************** MAC Interface(in/out) Equalization signals *********************/
+  input logic [18*`PCIE_LANE_NUMBER-1:0]   local_tx_preset_coeffcients,
+  input logic [18*`PCIE_LANE_NUMBER-1:0]   tx_deemph,
+  input logic [6*`PCIE_LANE_NUMBER-1:0]    local_fs,
+  input logic [6*`PCIE_LANE_NUMBER-1:0]    local_lf,
+  input logic [`PCIE_LANE_NUMBER-1:0]      get_local_preset_coeffcients,
+  input logic [`PCIE_LANE_NUMBER-1:0]      local_tx_coeffcients_valid,
+  input logic [6*`PCIE_LANE_NUMBER-1:0]    fs,    // TODO: Review specs for these values
+  input logic [6*`PCIE_LANE_NUMBER-1:0]    lf,    // TODO: Review specs for these values
+  input logic [`PCIE_LANE_NUMBER-1:0]      rx_eq_eval,
+  input logic [4*`PCIE_LANE_NUMBER-1:0]    local_preset_index,
+  input logic [`PCIE_LANE_NUMBER-1:0]      invalid_request,  // TODO: this signal needs to be checked
+  input logic [6*`PCIE_LANE_NUMBER-1:0]    link_evaluation_feedback_direction_change
+  /*************************************************************************************/
 );
 
   `include "uvm_macros.svh"
