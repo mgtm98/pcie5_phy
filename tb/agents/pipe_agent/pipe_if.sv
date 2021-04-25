5e1583cee3ed48e94e98b462c02b9f91be02e171
theMeanScore <m.ad.ali2165@gmail.com>
2021-04-25 20:49:27 +0200

    move the parameters to be all inside the parameters list

--- tb/agents/pipe_agent/pipe_if.sv
+++ tb/agents/pipe_agent/pipe_if.sv
@@ -1,33 +1,36 @@
 `include "settings.svh"
 
-interface pipe_if(
+interface pipe_if
+  #(
+    param pipe_num_of_lanes,
+    param pipe_max_width,
+    localparam bus_data_width_param       = pipe_num_of_lanes  * pipe_max_width - 1,  
+    localparam bus_data_kontrol_param     = (pipe_max_width / 8) * pipe_num_of_lanes - 1
+  )(
     input bit   clk,
     input bit   reset,
     input logic phy_reset
   );
 
-  localparam bus_data_width_param       = `PCIE_LANE_NUMBER  * `PIPE_MAX_WIDTH - 1;  
-  localparam bus_data_kontrol_param     = (`PIPE_MAX_WIDTH / 8) * `PCIE_LANE_NUMBER - 1;
-
   /*************************** RX Specific Signals *************************************/
   logic [bus_data_width_param:0]      rx_data;    
-  logic [`PCIE_LANE_NUMBER-1:0]       rx_data_valid;
+  logic [pipe_num_of_lanes-1:0]       rx_data_valid;
   logic [bus_data_kontrol_param:0]    rx_data_k;
-  logic [`PCIE_LANE_NUMBER-1:0]       rx_start_block;
-  logic [2*`PCIE_LANE_NUMBER-1:0]     rx_synch_header;
-  logic [`PCIE_LANE_NUMBER-1:0]       rx_valid;
-  logic [3*`PCIE_LANE_NUMBER-1:0]     rx_status;
+  logic [pipe_num_of_lanes-1:0]       rx_start_block;
+  logic [2*pipe_num_of_lanes-1:0]     rx_synch_header;
+  logic [pipe_num_of_lanes-1:0]       rx_valid;
+  logic [3*pipe_num_of_lanes-1:0]     rx_status;
   logic                               rx_elec_idle;
   /*************************************************************************************/
   
   /*************************** TX Specific Signals *************************************/
   logic [bus_data_width_param:0]      tx_data;    
-  logic [`PCIE_LANE_NUMBER-1:0]       tx_data_valid;
+  logic [pipe_num_of_lanes-1:0]       tx_data_valid;
   logic [bus_data_kontrol_param:0]    tx_data_k;
-  logic [`PCIE_LANE_NUMBER-1:0]       tx_start_block;
-  logic [2*`PCIE_LANE_NUMBER-1:0]     tx_synch_header;
-  logic [`PCIE_LANE_NUMBER-1:0]       tx_elec_idle;
-  logic [`PCIE_LANE_NUMBER-1:0]       tx_detect_rx__loopback;
+  logic [pipe_num_of_lanes-1:0]       tx_start_block;
+  logic [2*pipe_num_of_lanes-1:0]     tx_synch_header;
+  logic [pipe_num_of_lanes-1:0]       tx_elec_idle;
+  logic [pipe_num_of_lanes-1:0]       tx_detect_rx__loopback;
   /*************************************************************************************/
 
   /*********************** Comands and Status Signals **********************************/
@@ -45,17 +48,17 @@ interface pipe_if(
   /*************************************************************************************/
 
   /******************** MAC Interface(in/out) Equalization signals *********************/
-  logic [18*`PCIE_LANE_NUMBER-1:0]   local_tx_preset_coeffcients;
-  logic [18*`PCIE_LANE_NUMBER-1:0]   tx_deemph;
-  logic [6*`PCIE_LANE_NUMBER-1:0]    local_fs;
-  logic [6*`PCIE_LANE_NUMBER-1:0]    local_lf;
-  logic [`PCIE_LANE_NUMBER-1:0]      get_local_preset_coeffcients;
-  logic [`PCIE_LANE_NUMBER-1:0]      local_tx_coeffcients_valid;
-  logic [6*`PCIE_LANE_NUMBER-1:0]    fs;    // TODO: Review specs for these values
-  logic [6*`PCIE_LANE_NUMBER-1:0]    lf;    // TODO: Review specs for these values
-  logic [`PCIE_LANE_NUMBER-1:0]      rx_eq_eval;
-  logic [4*`PCIE_LANE_NUMBER-1:0]    local_preset_index;
-  logic [`PCIE_LANE_NUMBER-1:0]      invalid_request;
-  logic [6*`PCIE_LANE_NUMBER-1:0]    link_evaluation_feedback_direction_change;
+  logic [18*pipe_num_of_lanes-1:0]   local_tx_preset_coeffcients;
+  logic [18*pipe_num_of_lanes-1:0]   tx_deemph;
+  logic [6*pipe_num_of_lanes-1:0]    local_fs;
+  logic [6*pipe_num_of_lanes-1:0]    local_lf;
+  logic [pipe_num_of_lanes-1:0]      get_local_preset_coeffcients;
+  logic [pipe_num_of_lanes-1:0]      local_tx_coeffcients_valid;
+  logic [6*pipe_num_of_lanes-1:0]    fs;    // TODO: Review specs for these values
+  logic [6*pipe_num_of_lanes-1:0]    lf;    // TODO: Review specs for these values
+  logic [pipe_num_of_lanes-1:0]      rx_eq_eval;
+  logic [4*pipe_num_of_lanes-1:0]    local_preset_index;
+  logic [pipe_num_of_lanes-1:0]      invalid_request;
+  logic [6*pipe_num_of_lanes-1:0]    link_evaluation_feedback_direction_change;
   /*************************************************************************************/
 endinterface: pipe_if
