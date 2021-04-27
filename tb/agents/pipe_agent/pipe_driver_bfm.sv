8160b06126c9302342ea3d26e7890fc779a0b45c
Mohamed Gamal <mgtmprog@gmail.com>
2021-04-27 15:40:10 +0200

    fixed file format

--- tb/agents/pipe_agent/pipe_driver_bfm.sv
+++ tb/agents/pipe_agent/pipe_driver_bfm.sv
@@ -76,7 +76,7 @@ gen_t current_gen;
 // Methods
 //------------------------------------------
 
-  `include "link_up.svh"
+`include "link_up.svh"
 task automatic receive_ts (output TS_config ts ,input int start_lane = 0,input int end_lane = NUM_OF_LANES );
     if(Width==2'b01) // 16 bit pipe parallel interface
     begin
@@ -201,7 +201,6 @@ forever begin
 end
 
 task polling_state;
-
 	ts_t config_h;
 	//check array description
 	`uvm_info("Waiting for powerdown change on lane");
@@ -296,157 +295,157 @@ task polling_state;
 			send_ts(config_h);
 		end
 	join
-
 endtask : polling_state
 
-  task config_state;
-    ts_config_t received_tses [NUM_OF_LANES];
-    // -------------------- Config.Linkwidth.Start --------------------
-    int unsigned num_of_ts1s_with_non_pad_link_number [NUM_OF_LANES];
-    // Initialize the detected num of ts1s with non pad link number to zero
+task config_state;
+  ts_config_t received_tses [NUM_OF_LANES];
+  // -------------------- Config.Linkwidth.Start --------------------
+  int unsigned num_of_ts1s_with_non_pad_link_number [NUM_OF_LANES];
+  // Initialize the detected num of ts1s with non pad link number to zero
+  foreach(num_of_ts1s_with_non_pad_link_number[i])
+  begin
+    num_of_ts1s_with_non_pad_link_number[i] = 0;
+  end
+  // Detect ts1s until 2 consecutive ts1s have a non-pad link number
+  while(1)
+  begin
+    receive_ts(received_tses);
+    // Make sure the received tses are ts1s
+    foreach(received_tses[i])
+    begin
+      // Make sure the tses received are ts1s
+      assert (received_tses[i].ts_type == TS1) 
+      else   `uvm_error(get_name(), "Expected TS1s but detected TS2s")
+      // Non PAD link number
+      if(received_ts[i].use_link_number)
+      begin
+        num_of_ts1s_with_non_pad_link_number[i] += 1
+      end
+      // PAD link number
+      else
+      begin
+        num_of_ts1s_with_non_pad_link_number[i] = 0;
+      end
+    end
+    // Check if any lane detected 2 consecutive ts1s with non PAD link number
+    int unsigned two_consecutive_ts1s_with_non_pad_link_number_detected = 0;
     foreach(num_of_ts1s_with_non_pad_link_number[i])
     begin
-      num_of_ts1s_with_non_pad_link_number[i] = 0;
-    end
-    // Detect ts1s until 2 consecutive ts1s have a non-pad link number
-    while(1)
-    begin
-      receive_ts(received_tses);
-      // Make sure the received tses are ts1s
-      foreach(received_tses[i])
+      if(num_of_ts1s_with_non_pad_link_number[i] == 2)
       begin
-        // Make sure the tses received are ts1s
-        assert (received_tses[i].ts_type == TS1) 
-        else   `uvm_error(get_name(), "Expected TS1s but detected TS2s")
-        // Non PAD link number
-        if(received_ts[i].use_link_number)
-        begin
-          num_of_ts1s_with_non_pad_link_number[i] += 1
-        end
-        // PAD link number
-        else
-        begin
-          num_of_ts1s_with_non_pad_link_number[i] = 0;
-        end
-      end
-      // Check if any lane detected 2 consecutive ts1s with non PAD link number
-      int unsigned two_consecutive_ts1s_with_non_pad_link_number_detected = 0;
-      foreach(num_of_ts1s_with_non_pad_link_number[i])
-      begin
-        if(num_of_ts1s_with_non_pad_link_number[i] == 2)
-        begin
-          two_consecutive_ts1s_with_non_pad_link_number_detected = 1;
-          break;
-        end
-      end
-      // Move to the next sub-state if any lane detected 2 consecutive ts1s with non PAD link number
-      if(two_consecutive_ts1s_with_non_pad_link_number_detected)
-      begin
+        two_consecutive_ts1s_with_non_pad_link_number_detected = 1;
         break;
       end
     end
-
-    // -------------------- Config.Linkwidth.Accept --------------------
-    // Use the link number of the ts1s on the first lane to be transmitted
-    bit [7:0] used_link_num = ts_configs[0].link_number;
-    foreach(ts_configs[i])
+    // Move to the next sub-state if any lane detected 2 consecutive ts1s with non PAD link number
+    if(two_consecutive_ts1s_with_non_pad_link_number_detected)
     begin
-      ts_configs[i].link_number = used_link_num;
-      ts_configs[i].use_link_number = 1;
-      ts_configs[i].use_lane_number = 0;
+      break;
     end
-    // Send ts1s with this link number until some ts1s are received with non PAD lane number
-    ts1_with_non_pad_lane_number_detected = 0
-    fork
-      begin
-        while (!ts1_with_non_pad_lane_number_detected)
-        begin
-          send_ts(ts_configs);
-        end
-      end
-      begin
-        while (!ts1_with_non_pad_lane_number_detected)
-        begin
-          receive_ts(received_tses);
-          // Check if any ts1 received has a non PAD lane number
-          foreach(received_tses[i])
-          begin
-            // Make sure the tses received are ts1s
-            assert (received_tses[i].ts_type == TS1) 
-            else   `uvm_error(get_name(), "Expected TS1s but detected TS2s")
-            // Non PAD lane number
-            if(received_tses[i].use_lane_num)
-            begin
-              ts1_with_non_pad_lane_number_detected = 1
-            end
-          end
-        end
-      end
-    join
-    // Get the lane numbers from the received ts1s
-    foreach(received_tses[i])
+  end
+
+  // -------------------- Config.Linkwidth.Accept --------------------
+  // Use the link number of the ts1s on the first lane to be transmitted
+  bit [7:0] used_link_num = ts_configs[0].link_number;
+  foreach(ts_configs[i])
+  begin
+    ts_configs[i].link_number = used_link_num;
+    ts_configs[i].use_link_number = 1;
+    ts_configs[i].use_lane_number = 0;
+  end
+  // Send ts1s with this link number until some ts1s are received with non PAD lane number
+  ts1_with_non_pad_lane_number_detected = 0
+  fork
     begin
-      assert (received_tses[i].lane_number == i) 
-      else   `uvm_error(get_name(), "the order of lane numbers are incorrect")
-      ts_configs[i].lane_number = received_tses[i].lane_number;
-      ts_configs[i].use_lane_number = 1;
+      while (!ts1_with_non_pad_lane_number_detected)
+      begin
+        send_ts(ts_configs);
+      end
     end
-
-    // -------------------- Config.Lanenum.Wait --------------------
-    int num_of_ts2_received [NUM_OF_LANES];
-    // Initialize the num_of_ts2_received array with zeros
-    foreach(num_of_ts2_received[i])
     begin
-      num_of_ts2_received[i] = 0;
+      while (!ts1_with_non_pad_lane_number_detected)
+      begin
+        receive_ts(received_tses);
+        // Check if any ts1 received has a non PAD lane number
+        foreach(received_tses[i])
+        begin
+          // Make sure the tses received are ts1s
+          assert (received_tses[i].ts_type == TS1) 
+          else   `uvm_error(get_name(), "Expected TS1s but detected TS2s")
+          // Non PAD lane number
+          if(received_tses[i].use_lane_num)
+          begin
+            ts1_with_non_pad_lane_number_detected = 1
+          end
+        end
+      end
     end
-    // Transmit TS1s until 2 consecutive TS2s are received
-    int unsigned two_consecutive_ts2s_detected = 0;
-    fork
+  join
+  // Get the lane numbers from the received ts1s
+  foreach(received_tses[i])
+  begin
+    assert (received_tses[i].lane_number == i) 
+    else   `uvm_error(get_name(), "the order of lane numbers are incorrect")
+    ts_configs[i].lane_number = received_tses[i].lane_number;
+    ts_configs[i].use_lane_number = 1;
+  end
+
+  // -------------------- Config.Lanenum.Wait --------------------
+  int num_of_ts2_received [NUM_OF_LANES];
+  // Initialize the num_of_ts2_received array with zeros
+  foreach(num_of_ts2_received[i])
+  begin
+    num_of_ts2_received[i] = 0;
+  end
+  // Transmit TS1s until 2 consecutive TS2s are received
+  int unsigned two_consecutive_ts2s_detected = 0;
+  fork
+    begin
+      while (!two_consecutive_ts2s_detected)
       begin
-        while (!two_consecutive_ts2s_detected)
+        send_ts(ts_configs);
+      end
+    end
+
+    begin
+      while (!two_consecutive_ts2s_detected)
+      begin
+        receive_ts(received_tses);
+        foreach(received_tses[i])
         begin
-          send_ts(ts_configs);
+          if(received_tses[i].ts_type == TS2)
+          begin
+            num_of_ts2_received[i] += 1;
+          end
+          else
+          begin
+            num_of_ts2_received[i] = 0;
+          end
+        end
+        // Check if any lane detected 2 consecutive ts2s
+        foreach(num_of_ts2_received[i])
+        begin
+          if(num_of_ts2_received[i] == 2)
+          begin
+            two_consecutive_ts2s_detected = 1;
+          end
+        end
+        // Move to the next sub-state if any lane detected 2 consecutive ts2s
+        if(two_consecutive_ts2s_detected)
+        begin
+          break;
         end
       end
+    end
+  join
 
-      begin
-        while (!two_consecutive_ts2s_detected)
-        begin
-          receive_ts(received_tses);
-          foreach(received_tses[i])
-          begin
-            if(received_tses[i].ts_type == TS2)
-            begin
-              num_of_ts2_received[i] += 1;
-            end
-            else
-            begin
-              num_of_ts2_received[i] = 0;
-            end
-          end
-          // Check if any lane detected 2 consecutive ts2s
-          foreach(num_of_ts2_received[i])
-          begin
-            if(num_of_ts2_received[i] == 2)
-            begin
-              two_consecutive_ts2s_detected = 1;
-            end
-          end
-          // Move to the next sub-state if any lane detected 2 consecutive ts2s
-          if(two_consecutive_ts2s_detected)
-          begin
-            break;
-          end
-        end
-      end
-    join
+  // -------------------- Config.Lanenum.Accept --------------------
 
-    // -------------------- Config.Lanenum.Accept --------------------
+  // -------------------- Config.Complete --------------------
 
-    // -------------------- Config.Complete --------------------
-
-    // -------------------- Config.Idle --------------------
-  endtask
+  // -------------------- Config.Idle --------------------
+endtask
+  
 endinterface
 
 /*
