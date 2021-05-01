7dd6dfe8fc77d8637d66cf1a21db4e9d853d8fca
Mohamed Gamal <mgtmprog@gmail.com>
2021-05-01 16:22:24 +0200

    reordered compiling sequence, added HOME variable

--- tb/agents/lpif_agent/lpif_agent.mk
+++ tb/agents/lpif_agent/lpif_agent.mk
@@ -1,18 +1,24 @@
-LPIF_ROOT_PATH			?= .																# root of the files to be compiled
-LPIF_INCLUDE_DIR  	:= $(strip $(LPIF_ROOT_PATH))/../..	# include directory for the `include 
-LPIF_FILES					:=  																# files to be compiled
-COMPILE 						= $(strip $(LPIF_ROOT_PATH))/$(1) 	# used to append the root_path to the file_name
+LPIF_ROOT_PATH			?= .								# root of the files to be compiled
+LPIF_INCLUDE_DIR  		:= $(strip $(LPIF_ROOT_PATH))/../..	# include directory for the `include 
+LPIF_FILES_PACKAGE		:=  								# package files to be compiled
+LPIF_FILES_INTERFACE	:=  								# interface files to be compiled
+COMPILE 				= $(strip $(LPIF_ROOT_PATH))/$(1) 	# used to append the root_path to the file_name
 
 ################################## Files to be compiled ##########################################
-LPIF_FILES					+= $(call COMPILE,lpif_agent_pkg.sv)
-LPIF_FILES					+= $(call COMPILE,lpif_if.sv)
-LPIF_FILES					+= $(call COMPILE,lpif_driver_bfm.sv)
-LPIF_FILES					+= $(call COMPILE,lpif_monitor_bfm.sv)
+LPIF_FILES_PACKAGE						+= $(call COMPILE,lpif_agent_pkg.sv)
+LPIF_FILES_INTERFACE					+= $(call COMPILE,lpif_if.sv)
+LPIF_FILES_INTERFACE					+= $(call COMPILE,lpif_driver_bfm.sv)
+LPIF_FILES_INTERFACE					+= $(call COMPILE,lpif_monitor_bfm.sv)
 #################################################################################################
 
-LPIF_BUILD: 
-	@vlog +incdir+${LPIF_INCLUDE_DIR} ${LPIF_FILES}  -suppress 2275 -suppress 2286
-	@echo LPIF BUILD Done
+LPIF_BUILD_PACKAGE: 
+	@vlog +incdir+${UVM_HOME} +incdir+${LPIF_INCLUDE_DIR} ${LPIF_FILES_PACKAGE}  -suppress 2275 -suppress 2286
+	@echo LPIF Package BUILD Done
+	@echo ------------------------------------------------------------------------------------------
+
+LPIF_BUILD_INTERFACES:
+	@vlog +incdir+${UVM_HOME} +incdir+${LPIF_INCLUDE_DIR} ${LPIF_FILES_INTERFACE}  -suppress 2275 -suppress 2286
+	@echo LPIF Interfaces BUILD Done
 	@echo ------------------------------------------------------------------------------------------
 
 # 2275 --> warning for file already included and will be overwritten 
--- tb/agents/pipe_agent/pipe_agent.mk
+++ tb/agents/pipe_agent/pipe_agent.mk
@@ -1,18 +1,24 @@
-PIPE_ROOT_PATH			?= .																# root of the files to be compiled
-PIPE_INCLUDE_DIR  	:= $(strip $(PIPE_ROOT_PATH))/../..	# include directory for the `include 
-PIPE_FILES					:=  																# files to be compiled
-COMPILE 						= $(strip $(PIPE_ROOT_PATH))/$(1) 	# used to append the root_path to the file_name
+PIPE_ROOT_PATH			?= .								# root of the files to be compiled
+PIPE_INCLUDE_DIR  		:= $(strip $(PIPE_ROOT_PATH))/../..	# include directory for the `include 
+PIPE_FILES_PACKAGE		:=  								# package files to be compiled
+PIPE_FILES_INTERFACE	:=  								# interface files to be compiled
+COMPILE 				= $(strip $(PIPE_ROOT_PATH))/$(1) 	# used to append the root_path to the file_name
 
 ################################## Files to be compiled ##########################################
-PIPE_FILES					+= $(call COMPILE,pipe_agent_pkg.sv)
-PIPE_FILES					+= $(call COMPILE,pipe_if.sv)
-PIPE_FILES					+= $(call COMPILE,pipe_driver_bfm.sv)
-PIPE_FILES					+= $(call COMPILE,pipe_monitor_bfm.sv)
+PIPE_FILES_PACKAGE						+= $(call COMPILE,pipe_agent_pkg.sv)
+PIPE_FILES_INTERFACE					+= $(call COMPILE,pipe_if.sv)
+PIPE_FILES_INTERFACE					+= $(call COMPILE,pipe_driver_bfm.sv)
+# PIPE_FILES_INTERFACE					+= $(call COMPILE,pipe_monitor_bfm.sv)
 #################################################################################################
 
-PIPE_BUILD: 
-	@vlog +incdir+${PIPE_INCLUDE_DIR} ${PIPE_FILES}  -suppress 2275 -suppress 2286
-	@echo PIPE BUILD Done 
+PIPE_BUILD_PACKAGE: 
+	@vlog +incdir+${UVM_HOME} +incdir+${PIPE_INCLUDE_DIR} ${PIPE_FILES_PACKAGE}  -suppress 2275 -suppress 2286
+	@echo PIPE Pacakge BUILD Done 
+	@echo ------------------------------------------------------------------------------------------
+
+PIPE_BUILD_INTERFACES: 
+	@vlog +incdir+${UVM_HOME} +incdir+${PIPE_INCLUDE_DIR} ${PIPE_FILES_INTERFACE}  -suppress 2275 -suppress 2286
+	@echo PIPE Interfaces BUILD Done 
 	@echo ------------------------------------------------------------------------------------------
 
 # 2275 --> warning for file already included and will be overwritten 
