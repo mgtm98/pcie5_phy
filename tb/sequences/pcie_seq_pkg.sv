b2933b1a88cfb619761ce9d8c3b0439c36da4414
theMeanScore <m.ad.ali2165@gmail.com>
2021-05-21 10:54:24 +0200

    added the lpif and pipe data transmit sequences to pcie_seq_pkg

--- tb/sequences/pcie_seq_pkg.sv
+++ tb/sequences/pcie_seq_pkg.sv
@@ -11,20 +11,22 @@ package pcie_seq_pkg;
   `include "lpif_reset_seq.svh"
   `include "lpif_enter_recovery_seq.svh"
   `include "lpif_link_up_seq.svh"
+  `include "lpif_data_transmit_seq.svh"
 
   // PIPE Sequences
   `include "pipe_base_seq.svh"
   `include "pipe_reset_seq.svh"
-  // `include "pipe_link_up_seq.svh"
+  `include "pipe_link_up_seq.svh"
   `include "pipe_speed_change_seq.svh"
   `include "pipe_enter_recovery_seq.svh"
+  `include "pipe_data_transmit_seq.svh"
 
   // Virtual Sequences
   `include "base_vseq.svh"
   `include "reset_vseq.svh"
-  // `include "link_up_vseq.svh"
+  `include "link_up_vseq.svh"
   `include "enter_recovery_vseq.svh"
-  // `include "data_exchange_vseq.svh"
+  `include "data_exchange_vseq.svh"
 
 endpackage
 
