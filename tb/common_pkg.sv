bbce9ab6d0e228791b0ab4d72a65607f7d669726
theMeanScore <m.ad.ali2165@gmail.com>
2021-05-25 03:03:24 +0200

    Add the TLP_MIN_SIZE and TLP_MAX_SIZE to the common package

--- tb/common_pkg.sv
+++ tb/common_pkg.sv
@@ -1,7 +1,11 @@
 package common_pkg;
 
   // TLP and DLLP definitions
-  typedef bit [7:0] dllp_t [5:0];
+  typedef bit [7:0] dllp_t [0:5];
   typedef bit [7:0] tlp_t [];
+
+  // TLP and DLLP definitions
+  const longint unsigned TLP_MIN_SIZE = 12;
+  const longint unsigned TLP_MAX_SIZE = 4000;
 
 endpackage: common_pkg
