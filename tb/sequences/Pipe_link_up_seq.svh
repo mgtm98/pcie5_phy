4bf743903ed25223dd5b2f91f4079d68c5695bf6
theMeanScore <m.ad.ali2165@gmail.com>
2021-07-13 22:39:33 +0200

    Solve issue #62

--- tb/sequences/Pipe_link_up_seq.svh
+++ tb/sequences/Pipe_link_up_seq.svh
@@ -45,6 +45,7 @@ function pipe_link_up_seq::new(string name = "pipe_link_up_seq");
 endfunction
   
 task pipe_link_up_seq::body;
+  `uvm_info("pipe_link_up_seq", "Started pipe_link_up_seq", UVM_MEDIUM)
   if(!this.randomize()) begin
     `uvm_fatal(get_name(), "Can't randomize the pipe_link_up_seq")
   end
@@ -87,6 +88,7 @@ task pipe_link_up_seq::body;
   end
   config_state;
   -> pipe_agent_config_h.link_up_finished_e;
+  `uvm_info("pipe_link_up_seq", "Finished pipe_link_up_seq", UVM_MEDIUM)
 endtask: body
 
 task pipe_link_up_seq::detect_state;
@@ -502,6 +504,7 @@ task pipe_link_up_seq::config_idle_state_upstream;
       end
     end
   join
+  `uvm_info("pipe_link_up_seq", "Finished config_idle_state_upstream", UVM_MEDIUM)
 endtask
 
 task pipe_link_up_seq::config_linkwidth_start_state_downstream;
@@ -560,6 +563,7 @@ task pipe_link_up_seq::config_linkwidth_start_state_downstream;
       end
     end
   join
+  `uvm_info("pipe_link_up_seq", "Finished config_linkwidth_start_state_downstream", UVM_MEDIUM)
 endtask
 
 task pipe_link_up_seq::config_linkwidth_accept_state_downstream;
